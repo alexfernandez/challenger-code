e8d01b4ef02f9cc121cf57078c9c9cb14f3b4736
Alex Fernández <alexfernandeznpm@gmail.com>
2020-05-11 22:55:41 +0200

    Show more info about results.

--- public/js/run-challenge.js
+++ public/js/run-challenge.js
@@ -15,22 +15,34 @@ function sendDocument() {
 		method: 'POST',
 		body: JSON.stringify(body),
 		headers: {'content-type': 'application/json'},
-	}).then(response => {
-		if (response.status != 200) {
-			document.getElementById('result').innerHTML = `${response.status}: ${response.statusText}`
-			return
+	}).then(showResponse).catch(showError)
+}
+
+function showResponse(response) {
+	document.getElementById('submit').disabled = false
+	if (response.status != 200) {
+		document.getElementById('success').innerHTML = `${getSuccess(false)} ${response.status}: ${response.statusText}`
+		return
+	}
+	response.json().then(json => {
+		console.log(`Result: ${JSON.stringify(json)}`)
+		const text = `${getSuccess(json.success)} in ${json.elapsed} ms with ${json.nodes} nodes`
+		document.getElementById('success').innerHTML = text
+		for (let i = 0; i < json.results.length; i++) {
+			const result = json.results[i]
+			document.getElementById(`name${i}`).innerHTML = result.name
+			document.getElementById(`success${i}`).innerHTML = `${getSuccess(result.success)} in ${result.elapsed} ms`
 		}
-		response.json().then(json => {
-			console.log(`Result: ${JSON.stringify(json)}`)
-			document.getElementById('success').innerHTML = json.success
-			document.getElementById('elapsed').innerHTML = json.elapsed
-			for (let i = 0; i < json.results.length; i++) {
-				const result = json.results[i]
-				document.getElementById(`name${i}`).innerHTML = result.name
-				document.getElementById(`success${i}`).innerHTML = result.success
-				document.getElementById(`elapsed${i}`).innerHTML = result.elapsed
-			}
-		})
 	})
+}
+
+function getSuccess(success) {
+	if (success) return `✅ success`
+	return `❌ failure`
+}
+
+function showError(error) {
+	document.getElementById('submit').disabled = false
+	document.getElementById('success').innerHTML = `${getSuccess(false)} Could not send: ${error}`
 }
 
