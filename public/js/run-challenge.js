26f267ffac46fdebf98406de631df0b901899a9c
Alex Fernández <alexfernandeznpm@gmail.com>
2020-05-11 22:41:39 +0200

    Show rudimentary results.

--- public/js/run-challenge.js
+++ public/js/run-challenge.js
@@ -8,6 +8,7 @@ window.onload = () => {
 
 function sendDocument() {
 	console.log('sending')
+	document.getElementById('submit').disabled = true
 	const solution = document.getElementById('solution').value
 	const body = {code: solution}
 	fetch('/api/challenge/higher-than/run', {
@@ -19,7 +20,17 @@ function sendDocument() {
 			document.getElementById('result').innerHTML = `${response.status}: ${response.statusText}`
 			return
 		}
-		response.json().then(json => document.getElementById('result').innerHTML = JSON.stringify(json))
+		response.json().then(json => {
+			console.log(`Result: ${JSON.stringify(json)}`)
+			document.getElementById('success').innerHTML = json.success
+			document.getElementById('elapsed').innerHTML = json.elapsed
+			for (let i = 0; i < json.results.length; i++) {
+				const result = json.results[i]
+				document.getElementById(`name${i}`).innerHTML = result.name
+				document.getElementById(`success${i}`).innerHTML = result.success
+				document.getElementById(`elapsed${i}`).innerHTML = result.elapsed
+			}
+		})
 	})
 }
 
