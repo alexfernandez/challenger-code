d6ba8f3abd539df1ef3802f34758a26c0f219dc2
Alex Fernández <alexfernandeznpm@gmail.com>
2020-05-23 22:29:56 +0200

    Bug fixes when listing challenges.

--- test/challenge.js
+++ test/challenge.js
@@ -13,7 +13,7 @@ const wrong = 'function trash() {return 1}'
 const timeout = 200
 const parallel = `function solve() {return new Promise(resolve => setTimeout(() => resolve(0), ${timeout}))}`
 
-describe('Challenge integration tests', function() {
+describe.only('Challenge integration tests', function() {
 	this.timeout(10000)
 	let app = null
 	let headers = null
@@ -27,14 +27,14 @@ describe('Challenge integration tests', function() {
 		await server.stop(app)
 	})
 	it('should list challenges', async() => {
-		const challenges = await request.get(`${base}/challenges`)
+		const challenges = await request.get(`${base}/challenges/main`)
 		expect(challenges.length).to.be.above(2)
 		expect(challenges[0]).to.have.property('id')
 		expect(challenges[0]).to.have.property('name')
 	})
 	it('should list by category and difficulty', async() => {
 		for (const order of ['difficulty', 'category']) {
-			const challenges = await request.get(`${base}/challenges?order=${order}`)
+			const challenges = await request.get(`${base}/challenges/main?order=${order}`)
 			expect(challenges).to.have.property('test')
 			expect(challenges.test.length).to.be.above(0)
 			expect(challenges.test[0]).to.have.property('id')
@@ -42,7 +42,7 @@ describe('Challenge integration tests', function() {
 		}
 	})
 	it('should find a challenge', async() => {
-		const challenge = await request.get(`${base}/challenge/${challengeId}`)
+		const challenge = await request.get(`${base}/challenge/main/${challengeId}`)
 		checkChallenge(challenge, challengeId)
 	})
 	it('should not find non-existing challenge', async() => {
@@ -54,18 +54,18 @@ describe('Challenge integration tests', function() {
 		}
 	})
 	it('should run a challenge', async() => {
-		const run = await request.post(`${base}/challenge/${challengeId}/run`, {code}, headers)
+		const run = await request.post(`${base}/challenge/main/${challengeId}/run`, {code}, headers)
 		checkRun(run)
 		expect(run.success).to.equal(true)
 	})
 	it('should fail a challenge', async() => {
-		const run = await request.post(`${base}/challenge/${challengeId}/run`, {code: failure}, headers)
+		const run = await request.post(`${base}/challenge/main/${challengeId}/run`, {code: failure}, headers)
 		checkRun(run)
 		expect(run.success).to.equal(false)
 	})
 	it('should reject a challenge', async() => {
 		try {
-			await request.post(`${base}/challenge/${challengeId}/run`, {code: wrong}, headers)
+			await request.post(`${base}/challenge/main/${challengeId}/run`, {code: wrong}, headers)
 			throw new TestError('Should not run this trash')
 		} catch(error) {
 			expect(error.constructor.name).to.equal('RequestError')
@@ -74,7 +74,7 @@ describe('Challenge integration tests', function() {
 	it('should run challenges in parallel', async() => {
 		const promises = []
 		for (let i = 0; i < 5; i++) {
-			const promise = request.post(`${base}/challenge/${challengeId}/run`, {code: parallel}, headers)
+			const promise = request.post(`${base}/challenge/main/${challengeId}/run`, {code: parallel}, headers)
 			promises.push(promise)
 		}
 		const start = Date.now()
@@ -94,7 +94,7 @@ function checkChallenge(challenge, id) {
 	expect(challenge).to.have.property('name')
 	expect(challenge).to.not.have.property('implementation')
 	expect(challenge.verifications).to.be.an('array')
-	expect(challenge.verifications.length).to.equal(1)
+	expect(challenge.verifications.length).to.equal(2)
 }
 
 function checkRun(run) {
