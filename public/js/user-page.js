e7d41c8852993de0b5ceedfdef6db6d6a38a8a07
Alex Fernández <alexfernandeznpm@gmail.com>
2020-05-26 23:16:36 +0200

    Show create button on user page.

--- public/js/user-page.js
+++ public/js/user-page.js
@@ -5,6 +5,7 @@ window.loaders.push(() => {
 	const username = document.getElementById('username').innerText
 	if  (window.ccAuth && window.ccAuth.username == username) {
 		document.getElementById('personal').className = ''
+		document.getElementById('create').className = ''
 		const edits = document.getElementsByClassName('edit')
 		for (const edit of edits) {
 			edit.className = 'edit'
