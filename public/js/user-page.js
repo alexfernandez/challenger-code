f53d6a33f4b222f44369add2839f10bf2fbc1092
Alex Fernández <alexfernandeznpm@gmail.com>
2020-05-27 13:03:53 +0200

    Delete own challenges.

--- public/js/user-page.js
+++ public/js/user-page.js
@@ -6,14 +6,57 @@ window.loaders.push(() => {
 	if  (window.ccAuth && window.ccAuth.username == username) {
 		document.getElementById('personal').className = ''
 		document.getElementById('create').className = ''
-		const edits = document.getElementsByClassName('edit')
-		for (const edit of edits) {
-			edit.className = 'edit'
-		}
+		processClass('edit', makeVisible)
+		processClass('delete', makeVisible)
+		processClass('delete', element => element.onclick = () => {
+			const id = element.id.slice(element.id.indexOf('-') + 1)
+			deleteChallenge(username, id).catch(error => showError(error, id))
+		})
 	}
 	document.getElementById('logout').onclick = () => {
 		localStorage.removeItem('ccAuth')
 		window.location = '/'
 	}
 })
+
+function processClass(className, operation) {
+	const elements = document.getElementsByClassName(className)
+	for (const element of elements) {
+		operation(element)
+	}
+}
+
+function makeVisible(element) {
+	element.classList.remove('invisible')
+}
+
+async function deleteChallenge(username, id) {
+	const element = document.getElementById(`result-${id}`)
+	element.className = ''
+	element.innerText = ''
+	const name = document.getElementById(`name-${id}`).innerText
+	const sure = confirm(`Are you sure you want to delete challenge ${name}?`)
+	if (!sure) return
+	const response = await fetch(`/api/challenge/${username}/${id}`, {
+		method: 'DELETE',
+		headers: {
+			'content-type': 'application/json',
+			authorization: window.ccAuth.header,
+		},
+	})
+	if (response.status != 204) {
+		const json = await response.json()
+		showError(json.error, id)
+		return
+	}
+	element.className = 'success'
+	element.innerText = '✅ Challenge deleted'
+	setTimeout(() => window.location.reload(), 2000)
+}
+
+function showError(error, id) {
+	const element = document.getElementById(`result-${id}`)
+	element.className = 'errored'
+	element.innerText = `❌ Could not delete: ${error}`
+}
 
