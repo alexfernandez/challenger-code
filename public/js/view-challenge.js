a5d3a320c6bf23e0c46b62a7e46f20c61ae466a4
Alex Fernández <alexfernandeznpm@gmail.com>
2020-05-27 22:53:38 +0200

    Set up deletion from view page.

--- public/js/view-challenge.js
+++ public/js/view-challenge.js
@@ -4,6 +4,16 @@ window.loaders.push(() => {
 	const owner = document.getElementById('owner').innerText
 	if (window.ccAuth && window.ccAuth.username == owner) {
 		document.getElementById('edit').className = ''
+		document.getElementById('delete').className = ''
+		const deletion = new window.Deletion({
+			owner,
+			id: document.getElementById('id').innerText,
+			button: document.getElementById('delete'),
+			name: document.getElementById('name').innerText,
+			result: document.getElementById('result'),
+			success: () => history.back(),
+		})
+		deletion.setup()
 	}
 })
 
